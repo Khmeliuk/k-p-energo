5380cb6fc89b19c29daa719966f838e0f457c551
Артем Хмелюк <you@example.combigsmash@ukr.net  git config --global user.name Khmeliuk>
2025-07-20 17:40:00 +0300

    fix port and npm start

--- server/app.js
+++ server/app.js
@@ -5,8 +5,11 @@ import "dotenv/config";
 // server fastify
 const server = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT || 3000 });
-    fastify.log.info(`server start at port ${fastify.server.address().port}`);
+    const address = await fastify.listen({
+      port: process.env.PORT || 3000,
+      host: "0.0.0.0",
+    });
+    fastify.log.info(`Server started at ${address}`);
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
