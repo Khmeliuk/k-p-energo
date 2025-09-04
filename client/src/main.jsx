c1f7394d8026a4d91b6125a983f5fa22238a6bef
Khmeliuk <bigsmash@ukr.net>
2025-09-04 09:55:46 +0300

    context reaact delete

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,14 @@ import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
 import { ThemeProvider as SCThemeProvider } from "styled-components";
 import App from "./App";
 import theme from "./theme";
-import { MyProvider } from "./context/reactContext";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <MUIThemeProvider theme={theme}>
     <SCThemeProvider theme={theme}>
-      <MyProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </MyProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </SCThemeProvider>
   </MUIThemeProvider>
 );
